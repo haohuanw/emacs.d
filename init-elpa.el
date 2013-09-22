8732d06e617c742f4b8f4dcb284a0145b28d95ba
Haohuan Wang <haohuanw@umich.edu>
2013-09-22 19:45:29 -0400

    remove confluence-edit dependency

--- init-elpa.el
+++ init-elpa.el
@@ -268,7 +268,7 @@ ARCHIVE is the string name of the package archive.")
 (require-package 'dired-details)
 (require-package 'popwin)
 (require-package 'elnode)
-(require-package 'confluence-edit)
+;;(require-package 'confluence-edit)
 ;;(require-package 'git-messenger '(20130613 1222 0) nil)
 (require-package 'issue-tracker '(0 0 1) nil)
 
